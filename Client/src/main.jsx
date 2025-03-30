c9157e2fc878fab3cb57495a64907355c49fcce0
narmadavivek <narmada.viveka@gmail.com>
2025-03-30 22:23:51 +0530

    mern-recipe

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App_State from './context/App_State.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
